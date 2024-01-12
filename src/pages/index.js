f0c6a2a6f524e85527afa69b1836f97ff2fae9f0
mt-romero <mariateresa.romero@getonbrd.com>
2024-01-12 16:22:08 -0500

    fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <section className='flex flex-col p-4 gap-4 items-center'>
+        <section className='flex flex-col p-4 gap-4 items-center justify-center mt-10'>
             <div class="w-full flex flex-col items-center p-4 gap-2">
                 <h1 className='font-bold text-3xl'>Calcula tu seguro(?)</h1>
                 <p className='text-md text-neutral-500'>Alguna descripción de algún tipo</p>
