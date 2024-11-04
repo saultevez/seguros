f8d16a4afa3961ff1410413835fd9296b5e20b90
saultevez <47807433+saultevez@users.noreply.github.com>
2024-11-04 07:47:17 -0500

    cambio de texto

--- src/pages/Calculator/FormularioSalud/components/discountOptions.js
+++ src/pages/Calculator/FormularioSalud/components/discountOptions.js
@@ -1,7 +1,7 @@
 
 const discountOptions = [
-    { id: 'si_descuento', label: <p className="flex flex-col text-slate-800/80"><span className="font-bold text-slate-700">Si</span>Tengo un seguro actualmente</p>, value: 'si' },
-    { id: 'no-ahora_descuento', label: <p className="flex flex-col text-slate-800/80"><span className="font-bold text-slate-700">Actualmente no</span>Pero tuve hace menos de 90 días.</p>, value: 'no-ahora' }, 
-    { id: 'no_descuento', label: <p className="flex flex-col text-slate-800/80"><span className="font-bold text-slate-700">No</span>Nunca he tenido o no desde hace 90 días o más</p>, value: 'no' }]
+    { id: 'si_descuento', label: <p className="flex flex-col text-slate-800/80"><span className="font-bold text-slate-700">Si</span>Tengo un seguro actualmente.</p>, value: 'si' },
+    { id: 'no-ahora_descuento', label: <p className="flex flex-col text-slate-800/80"><span className="font-bold text-slate-700">Actualmente no</span>Tuve hace menos de 90 días.</p>, value: 'no-ahora' }, 
+    { id: 'no_descuento', label: <p className="flex flex-col text-slate-800/80"><span className="font-bold text-slate-700">No</span>Nunca he tenido o no desde hace 90 días o más.</p>, value: 'no' }]
 
 export default discountOptions
--- src/pages/Calculator/FormularioSalud/components/insuranceOptions.js
+++ src/pages/Calculator/FormularioSalud/components/insuranceOptions.js
@@ -1,10 +1,10 @@
 
 const insuranceOptions = [
-    { id: 'super economico', label: <p className="flex flex-col gap-0 text-slate-800/80"><span className="font-bold text-slate-700">Super económico</span> Cobertura integral con cobertura oncológica sobre 80% </p>, value: 'superEconomico' },
-    { id: 'economico', label: <p className="flex flex-col gap-0 text-slate-800/80"><span className="font-bold text-slate-700">Económico</span> Cobertura Integral y oncológica al 100% </p>, value: 'economico' },
-    { id: 'basico', label: <p className="flex flex-col gap-0 text-slate-800/80"><span className="font-bold text-slate-700">Básico</span> Cobertura esencial que cubre las necesidades médicas más comunes, proporcionando acceso a atención sanitaria de calidad a un precio accesible. </p>, value: 'basico' },
-    { id: 'completo', label: <p className="flex flex-col gap-0 text-slate-800/80"><span className="font-bold text-slate-700">Completo</span> Cobertura Integral y Oncológica al 100% en todas las clínicas a nivel nacional </p>, value: 'completo' },
-    { id: 'gama alta', label: <p className="flex flex-col gap-0 text-slate-800/80"><span className="font-bold text-slate-700">Gama alta</span> Cobertura Integral y Oncológica al 100% en todas las clínicas a nivel nacional e internacional </p>, value: 'gamaAlta' }
+    { id: 'super economico', label: <p className="flex flex-col gap-0 text-slate-800/80"><span className="font-bold text-slate-700">Muy económico</span> Cobertura integral con cobertura oncológica en la mayoría de los casos sobre el 80%. </p>, value: 'superEconomico' },
+    { id: 'economico', label: <p className="flex flex-col gap-0 text-slate-800/80"><span className="font-bold text-slate-700">Básico</span> Cobertura integrales y oncológicas al 100%. </p>, value: 'economico' },
+    { id: 'basico', label: <p className="flex flex-col gap-0 text-slate-800/80"><span className="font-bold text-slate-700">Completo</span> Cobertura integral y oncológica al 100% en una buena variedad de clínicas. </p>, value: 'basico' },
+    { id: 'completo', label: <p className="flex flex-col gap-0 text-slate-800/80"><span className="font-bold text-slate-700">Gama media-alta</span> Cobertura integral y oncológica al 100% en casi todas las clínicas a nivel nacional. </p>, value: 'completo' },
+    { id: 'gama alta', label: <p className="flex flex-col gap-0 text-slate-800/80"><span className="font-bold text-slate-700">Gama alta</span> Cobertura integral y oncológica a nivel nacional e internacional. </p>, value: 'gamaAlta' }
 ]
 
 export default insuranceOptions
